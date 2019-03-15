8c304230e465be015bda04b0341e519753cf7cd8
gerda <danielamsimoes01@gmail.com>
2019-03-15 16:12:57 +0100

    initialised spring

---
+++ demo/src/main/java/ch/ny/demo/TrialDaysApplication.java
@@ -0,0 +1,13 @@
+package ch.ny.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialDaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialDaysApplication.class, args);
+	}
+
+}
