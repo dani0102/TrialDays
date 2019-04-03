51eb7a2222632a86746a8e41a34153d09f94b892
Duarte Ramos <duarte.ramos@nyp.ch>
2019-04-03 16:08:34 +0200

    Changed Inheritance Strategy

--- demo/src/main/java/ch/ny/demo/person/Person.java
+++ demo/src/main/java/ch/ny/demo/person/Person.java
@@ -4,21 +4,24 @@ import java.time.LocalDate;
 import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.Type;
 
 import ch.ny.demo.role.Role;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Person {
 
 	public Person() { }
